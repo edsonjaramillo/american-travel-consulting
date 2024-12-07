7f1e5ab6cc8dd04d15c377664bb4917caaf84f1c
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-12-07 12:14:27 -0600

    feat: enhance DesktopNavigation with Logo and Register components

--- src/app/components/navigation/DesktopNavigation.tsx
+++ src/app/components/navigation/DesktopNavigation.tsx
@@ -1,19 +1,22 @@
 import Link from 'next/link';
 
+import { Logo } from '@/app/components/common/Logo';
+import { Register } from '@/app/components/navigation/Register';
 import { Responsive } from '@/app/components/ui/Responsive';
+import { Text } from '@/app/components/ui/Text';
 import { type NavigationLink, navigationLinks } from '@/app/data/links';
 
 export function DesktopNavigation() {
   return (
-    <nav>
+    <nav className="hidden shadow-md md:block">
       <Responsive className="flex h-14 items-center justify-between">
-        <p>Logo</p>
+        <Logo />
         <ul className="flex space-x-4">
           {navigationLinks.map((link) => (
             <DesktopLink key={link.href} link={link} />
           ))}
         </ul>
-        <p>Register Button</p>
+        <Register />
       </Responsive>
     </nav>
   );
@@ -23,8 +26,13 @@ type DesktopLinksProperties = { link: NavigationLink };
 function DesktopLink({ link }: DesktopLinksProperties) {
   return (
     <li>
-      <Link href={link.href} prefetch={link.prefetch}>
-        {link.name}
+      <Link
+        href={link.href}
+        prefetch={link.prefetch}
+        className="font-medium text-tertiary-dark transition-colors duration-base hover:text-secondary">
+        <Text as="span" size="small">
+          {link.name}
+        </Text>
       </Link>
     </li>
   );
