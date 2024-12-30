552cc1c0aca50726280b319886fa770040868593
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-12-30 16:29:57 -0600

    feat: fetch and display dynamic page image in LoginPage component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,8 @@
+import { cms } from '@/cms/clients/CMSClient';
 import { LoginForm } from '@/components/forms/LoginForm';
 import { SplitShell } from '@/components/ui/SplitShell';
 
-export default function LoginPage() {
-  return (
-    <SplitShell
-      image={{ src: 'https://picsum.photos/id/237/400/800', alt: 'gfgfgf' }}
-      form={<LoginForm />}
-    />
-  );
+export default async function LoginPage() {
+  const pageImage = await cms.getPageImage('login');
+  return <SplitShell image={{ src: pageImage.image.url, alt: 'gfgfgf' }} form={<LoginForm />} />;
 }
