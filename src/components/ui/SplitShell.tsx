d28df4ad298644aef6bdae3c26d3c4e1cf7dceca
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-12-30 16:30:14 -0600

    fix: adjust padding and minimum height in SplitShell component for better layout

--- src/components/ui/SplitShell.tsx
+++ src/components/ui/SplitShell.tsx
@@ -9,17 +9,16 @@ type SplitShellProps = React.ComponentProps<'div'> & {
 
 export function SplitShell({ image, form, children, className, ...props }: SplitShellProps) {
   const { className: imgClassNames, ...imgProps } = image;
-
   return (
     <div className="flex flex-col-reverse md:grid md:grid-cols-2" {...props}>
       <div
         className={cn(
-          'mx-auto w-responsive md:ml-auto md:mr-0 md:w-half-responsive [&>div]:md:px-8',
+          'mx-auto w-responsive md:ml-auto md:mr-0 md:w-half-responsive [&>div]:md:px-16',
           className,
         )}>
         {form}
       </div>
-      <div className="relative h-[25rem] md:h-auto">
+      <div className="relative h-[25rem] md:h-auto md:min-h-[45rem]">
         <Image className={cn('h-full w-full object-cover', imgClassNames)} fill {...imgProps} />
         {children}
       </div>
