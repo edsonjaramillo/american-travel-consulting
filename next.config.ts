5b1172ca5a815ba6fb6e029e118bf928b4de3ed8
Edson Jaramillo <edsonajaramillo@gmail.com>
2026-05-05 19:25:34 -0500

    updated next to fix security issues from vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from 'next';
 
-const isDev = process.env.NODE_ENV === 'development';
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: 'picsum.photos' }, { hostname: 'media.graphassets.com' }],
   },
-  experimental: { reactCompiler: isDev },
 };
 
 export default nextConfig;
